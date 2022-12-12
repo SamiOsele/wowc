ea3c175c0bbca6ab69b2a404b229d7eb3836fa2f
SamiOsele <Samiosele23@gmail.com>
2022-12-12 08:12:32 +0100

    dick sucking is nice

--- src/main/java/com/mycompany/editor/User.java
+++ src/main/java/com/mycompany/editor/User.java
@@ -12,16 +12,67 @@ import java.util.Set;
  */
 public class User {
     
-    private String username;
-    private String password;
-    private Set<Character> characterSet;
+    private String username, password, description, status;
+    private int level, xp;
+    private String lastlogon, createdate;
 
-    public User(){}
-
-    public User(String username, String password, Set<Character> characterSet) {
+    public User(String username, String password, String description, String status, int level, int xp, String lastlogon, String createdate) {
         this.username = username;
         this.password = password;
-        this.characterSet = characterSet;
+        this.description = description;
+        this.status = status;
+        this.level = level;
+        this.xp = xp;
+        this.lastlogon = lastlogon;
+        this.createdate = createdate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getXp() {
+        return xp;
+    }
+
+    public void setXp(int xp) {
+        this.xp = xp;
+    }
+
+    public String getLastlogon() {
+        return lastlogon;
+    }
+
+    public void setLastlogon(String lastlogon) {
+        this.lastlogon = lastlogon;
+    }
+
+    public String getCreatedate() {
+        return createdate;
+    }
+
+    public void setCreatedate(String createdate) {
+        this.createdate = createdate;
     }
 
     public String getUsername() {
@@ -30,14 +81,6 @@ public class User {
 
     public String getPassword() {
         return password;
-    }
-
-    public Set<Character> getCharacterSet() {
-        return characterSet;
-    }
-
-    public void setCharacterSet(Set<Character> characterSet) {
-        this.characterSet = characterSet;
     }
 
     public void setUsername(String username) {
